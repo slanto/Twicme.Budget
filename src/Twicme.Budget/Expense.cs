4296fdf033c870d11cd2f9dabda22c86c74cddf9
slanto <slawek.antos@gmail.com>
2018-12-26 17:14:21 +0100

    IClock used in Expense constructor

--- src/Twicme.Budget/Expense.cs
+++ src/Twicme.Budget/Expense.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Twicme.Budget
 {
     public class Expense
     {
-        public DateTimeOffset Datetime { get; }
+        public DateTimeOffset Created { get; }
         public Money Money { get; }
         public string Description { get; }
 
-        public Expense(DateTimeOffset datetime, Money money, string description)
+        public Expense(Money money, IClock clock, string description)
         {
-            Datetime = datetime;
+            Created = clock.UtcNow;
             Money = money;
             Description = description;
+        }
+
+        public Expense(Money money, IClock clock) : this(money, clock, string.Empty)
+        {
         }
     }
 }
--- tests/Twicme.Budget.Tests/ExpenseTests.cs
+++ tests/Twicme.Budget.Tests/ExpenseTests.cs
@@ -1,17 +1,31 @@
 using System;
 using FluentAssertions;
+using NSubstitute;
 using Xunit;
 
 namespace Twicme.Budget.Tests
 {
     public class ExpenseTests
     {
+        private readonly IClock _clock;
+        private static DateTimeOffset Now => new DateTimeOffset(2019, 1, 1, 10, 20, 0, TimeSpan.Zero);
+        
+        public ExpenseTests()
+        {
+            _clock = Substitute.For<IClock>();
+            _clock.UtcNow.Returns(Now);
+        }
+        
         [Fact]
         public void GivenCorrectInputData_WhenConstructorIsCalled_ThenExpenseIsCreated()
         {
-            var sut = new Expense(DateTimeOffset.UtcNow, Money.CreateZloty(10.12M), "phone bill");
+            var money = Money.CreateZloty(10.12M);
+            const string description = "phone bill";
+            var sut = new Expense(money, _clock, description);
 
             sut.Should().NotBeNull("expense is created");
+            sut.Money.Should().BeEquivalentTo(money);
+            sut.Created.Should().Be(Now);
         }
     }
 }
