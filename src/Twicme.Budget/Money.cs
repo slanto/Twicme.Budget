82936cf6efdec2d0e3740d5e51739779a5687885
slanto <slawek.antos@gmail.com>
2018-12-30 19:32:19 +0100

    Add and subtract money operations implemented

--- src/Twicme.Budget/Money.cs
+++ src/Twicme.Budget/Money.cs
@@ -5,24 +5,52 @@ namespace Twicme.Budget
 {
     public class Money : ValueObject<Money>
     {
+        public const string PLNCurrency = "PLN";
+        public const string USDCurrency = "USD";
+        
         public decimal Amount { get; }
         public string Currency { get; }
 
         private Money(decimal amount, string currency)
         {
-            Amount = amount;
+            Amount = decimal.Round(amount, 2);
             Currency = currency;
         }
 
         public static Money CreateZloty(decimal amount)
         {   
-            return new Money(amount, "PLN");
+            return new Money(amount, PLNCurrency);
         }
 
+        public static Money CreateDollar(decimal amount)
+        {   
+            return new Money(amount, USDCurrency);
+        }
+        
         protected override IEnumerable<object> GetEqualityComponents()
         {
             yield return Amount;
             yield return Currency;
+        }
+
+        public static Money operator +(Money elem1, Money elem2)
+        {
+            if (elem1.Currency != elem2.Currency)
+            {
+                throw new Exception("Cannot add money with different currency");
+            }
+            
+            return new Money(elem1.Amount + elem2.Amount, elem1.Currency);
+        }
+        
+        public static Money operator -(Money minuend, Money subtrahend)
+        {
+            if (minuend.Currency != subtrahend.Currency)
+            {
+                throw new Exception("Cannot subtract money with different currency");
+            }
+            
+            return new Money(minuend.Amount + subtrahend.Amount, minuend.Currency);
         }
     }
 }
--- tests/Twicme.Budget.Tests/MoneyTests.cs
+++ tests/Twicme.Budget.Tests/MoneyTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
@@ -11,7 +13,65 @@ namespace Twicme.Budget.Tests
             var sut = Money.CreateZloty(10);
 
             sut.Amount.Should().Be(10);
-            sut.Currency.Should().Be("PLN");
+            sut.Currency.Should().Be(Money.PLNCurrency);
+        }
+
+        [Fact]
+        public void GivenValueWithHighPrecision_WhenConstructorIsCalled_ThenMoneyIsCreatedWithTwoDecimalPlace()
+        {
+            var sut = Money.CreateZloty(0.2678765678M);
+
+            sut.Amount.Should().Be(0.27M);
+        }
+        
+        public static IEnumerable<object[]> PlusOperatorTestCases()
+        {
+            yield return new object[]
+            {
+                Money.CreateZloty(12),
+                Money.CreateZloty(8),
+                Money.CreateZloty(20)
+            };
+            
+            yield return new object[]
+            {
+                Money.CreateZloty(0),
+                Money.CreateZloty(1433),
+                Money.CreateZloty(1433)
+            };
+            
+            yield return new object[]
+            {
+                Money.CreateZloty(0.25M),
+                Money.CreateZloty(0.123456789M),
+                Money.CreateZloty(0.37M)
+            };
+            
+            yield return new object[]
+            {
+                Money.CreateZloty(0.99M),
+                Money.CreateZloty(1000),
+                Money.CreateZloty(1000.99M)
+            };
+        }
+            
+        [Theory, MemberData(nameof(PlusOperatorTestCases))]
+        public void GivenElements_WhenPlusOperatorIsCalled_ThenSumIsCalculated(Money elem1, Money elem2, Money sum)
+        {
+            var result = elem1 + elem2;
+            
+            result.Should().BeEquivalentTo(sum);
+        }
+
+        [Fact]
+        public void GivenElementsWithDifferentCurrency_WhenSumIsCalculated_ThenExceptionIsThrown()
+        {
+            var zloty = Money.CreateZloty(1);
+            var dollar = Money.CreateDollar(1);
+            
+            Func<Money> act = () => zloty + dollar;
+
+            act.Should().Throw<Exception>().WithMessage("Cannot add money with different currency");
         }
     }
 }
