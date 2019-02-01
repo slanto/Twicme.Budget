dd9813924e7bc540439af86f51c32277763f534a
slanto <slawek.antos@gmail.com>
2019-02-01 19:27:09 +0100

    Budget CLI initialized

---
+++ src/Twicme.Budget.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Twicme.Budget.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
