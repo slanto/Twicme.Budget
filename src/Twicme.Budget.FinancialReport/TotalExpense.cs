f76fcd1297869bbb9c4c2375f78c3e5e1bd664be
slanto <slawek.antos@gmail.com>
2019-02-05 23:21:55 +0100

    TotalExpense calculated based on planned and actual budgets

--- src/Twicme.Budget.FinancialReport/TotalExpense.cs
+++ src/Twicme.Budget.FinancialReport/TotalExpense.cs
@@ -6,21 +6,38 @@ namespace Twicme.Budget.FinancialReport
 {
     public class TotalExpense
     {
-        private readonly Currency _currency;
-        private ImmutableList<Money> _expenses;
-        
+        private readonly Budget _plannedBudget;
+        private readonly Budget _actualBudget;
+
+        private ImmutableList<Money> _plannedExpenses;
+        private ImmutableList<Money> _actualExpenses;
         public TotalExpense(Budget budget)
         {
-            _currency = budget.BaseCurrency;
-            _expenses = budget.Expenses();
+            _plannedBudget = budget;
+            _plannedExpenses = budget.Expenses();
         }
 
+        public TotalExpense(Budget plannedBudget, Budget actualBudget)
+        {
+            _plannedBudget = plannedBudget;
+            _actualBudget = actualBudget;
+
+            _plannedExpenses = _plannedBudget.Expenses();
+            _actualExpenses = _actualBudget.Expenses();
+        }
+
+        public Amount Amount =>
+            _actualExpenses == null
+                ? _plannedExpenses.Sum(_plannedBudget.BaseCurrency)
+                : _actualExpenses.Sum(_actualBudget.BaseCurrency) -
+                  _plannedExpenses.Sum(_plannedBudget.BaseCurrency);
+        
         public TotalExpense For(Func<Money, bool> predicate)
         {
-            _expenses = _expenses.Where(predicate).ToImmutableList();
+            _plannedExpenses = _plannedExpenses.Where(predicate).ToImmutableList();
+            _actualExpenses = _actualExpenses?.Where(predicate).ToImmutableList();
+            
             return this;
         }
-
-        public Amount Amount => _expenses.Sum(_currency);
     }
 }
--- tests/Twicme.Budget.Tests/TotalExpenseTests.cs
+++ tests/Twicme.Budget.Tests/TotalExpenseTests.cs
@@ -29,5 +29,26 @@ namespace Twicme.Budget.Tests
             totalExpense.For(e => e.Category == Category.HomeAndBills)
                 .Amount.Should().Be(Amount.Create(-50.55M, _budget.BaseCurrency));
         }
+        
+        [Fact]
+        public void
+            GivenPlannedAndActualBudgets_WhenTotalExpenseIsCalled_ThenTotalExpenseIsCalculated()
+        {
+            var plannedBudget = new Budget(Month.Create(2018, MonthName.May), Currency.PLN)
+                .WithRevenue(new Money(Amount.Create(100, Currency.PLN), Category.Salary))
+                .WithRevenue(new Money(Amount.Create(100, Currency.PLN), Category.Salary))
+                .WithExpense(new Money(Amount.Create(-10, Currency.PLN), Category.BasicExpenditure))
+                .WithExpense(new Money(Amount.Create(-60, Currency.PLN), Category.CarAndTransport));
+            
+            var actualBudget = new Budget(Month.Create(2018, MonthName.May), Currency.PLN)
+                .WithRevenue(new Money(Amount.Create(100.90m, Currency.PLN), Category.Salary))
+                .WithRevenue(new Money(Amount.Create(90, Currency.PLN), Category.Salary))
+                .WithExpense(new Money(Amount.Create(-8, Currency.PLN), Category.BasicExpenditure))
+                .WithExpense(new Money(Amount.Create(-50, Currency.PLN), Category.CarAndTransport));
+            
+            var totalExpense = new TotalExpense(plannedBudget, actualBudget);
+
+            totalExpense.Amount.Should().Be(Amount.Create(12, Currency.PLN));
+        }
     }
 }
