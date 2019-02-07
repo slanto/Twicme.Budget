c4bc45706b482d8c69a3597c38726af480f5e127
slanto <slawek.antos@gmail.com>
2019-02-07 21:41:48 +0100

    cli commands design introduced

--- src/Twicme.Budget.Cli/Program.cs
+++ src/Twicme.Budget.Cli/Program.cs
@@ -7,6 +7,16 @@ namespace Twicme.Budget.Cli
     {
         const string HelpFlagTemplate = "-? |-h |--help";
         
+        // app create -y 2019 -m 5 -c pln -> creates budget for May in 2019 in PLN
+        // app list -> displays all available budgets ordered by year and month descending:
+        // 1. 2019-05
+        // 2. 2019-04
+        // 3. 2018-01
+        
+        // app budget plan 2019-05 -> loads planned budged for May 2019
+        // app budget plan add revenue -a 124.55 -t salary -d "my wife's salary"
+        // app budget plan add expense -a 124.55 -t car -d "petrol"
+            
         static void Main(string[] args)
         {
             try
@@ -53,10 +63,7 @@ namespace Twicme.Budget.Cli
                 });
             }, false);
 
-            
-
             return app.Execute(args);
-
         }
     }
 }
