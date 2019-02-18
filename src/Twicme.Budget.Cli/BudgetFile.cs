d74a02ed907245948efc70aad1ef87c2b47f8644
slanto <slawek.antos@gmail.com>
2019-02-18 20:26:36 +0100

    Budget file introduced

---
+++ src/Twicme.Budget.Cli/BudgetFile.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using Twicme.Budget.Store;
+
+namespace Twicme.Budget.Cli
+{
+    public class BudgetFile
+    {
+        private readonly Budget _budget;
+        private readonly FileName _fileName;
+
+        public BudgetFile(Budget budget, FileName fileName)
+        {
+            _budget = budget;
+            _fileName = fileName;
+        }
+
+        public void Save() =>
+            File.WriteAllText(_fileName.Path,
+                new JsonBudget(_budget).Content.Value);
+        
+        public bool NotExists() =>
+            !FileName.Real(_budget).Exists || !FileName.Planned(_budget).Exists;
+    }
+}
--- src/Twicme.Budget.Cli/CreateBudgetCommandBuilder.cs
+++ src/Twicme.Budget.Cli/CreateBudgetCommandBuilder.cs
@@ -25,22 +25,41 @@ namespace Twicme.Budget.Cli
                 config.Description = "create budget providing year, month and currency";
                 config.HelpOption(HelpFlagTemplate);          
                
-                config.OnExecute(() =>
-                {
-                    var yearOption = YearOption.Create(config);
-                    var monthOption = MonthOption.Create(config);
-                    var currencyOption = CurrencyOption.Create(config);
-                    
-                    if (yearOption.NotExists || monthOption.NotExists || currencyOption.NotExists)
-                    {
-                        config.ShowHelp();
-                        return 1;
-                    }
-
-                    var service = new CreateBudgetService(new ConsoleLog());
-                    return service.Execute(yearOption.Value, monthOption.Value, currencyOption.Value);
-                });
+                config.OnExecute(() => Execute(config, new ConsoleLog()));
             }, false);    
+        }
+
+        private int Execute(CommandLineApplication config, ILog log)
+        {
+            var yearOption = YearOption.Create(config);
+            var monthOption = MonthOption.Create(config);
+            var currencyOption = CurrencyOption.Create(config);
+                    
+            if (yearOption.NotExists || monthOption.NotExists || currencyOption.NotExists)
+            {
+                config.ShowHelp();
+                return 1;
+            }
+            
+            var month = Month.Create(yearOption.Value, MonthName.Create(monthOption.Value));
+            var baseCurrency = Currency.Create(currencyOption.Value);
+            
+            var budget = new Budget(month, baseCurrency);
+
+            var plannedBudgetFile = new BudgetFile(budget, FileName.Planned(budget));
+            var realBudgetFile = new BudgetFile(budget, FileName.Real(budget));
+            
+            if (plannedBudgetFile.NotExists() || realBudgetFile.NotExists())
+            {
+                plannedBudgetFile.Save();
+                realBudgetFile.Save();
+                
+                log.Write($"Budget {budget} has been created.");
+                return 0;
+            }
+
+            log.Write($"Budget {budget} already exists.");
+            return 1;
         }
     }
 }
