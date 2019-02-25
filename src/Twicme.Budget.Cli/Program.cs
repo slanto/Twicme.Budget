800a5f8a7283fe184c2810326edd5dfaccc722be
slanto <slawek.antos@gmail.com>
2019-02-25 22:35:32 +0100

    Add and create commands moved to program for debugging purpose

--- src/Twicme.Budget.Cli/Program.cs
+++ src/Twicme.Budget.Cli/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using Microsoft.Extensions.CommandLineUtils;
 using Twicme.Budget.Cli.CommandBuilders;
+using Twicme.Budget.Cli.Options;
 
 namespace Twicme.Budget.Cli
 {
     class Program
-    {
-        const string HelpFlagTemplate = "-? |-h |--help";
-           
+    {   
         static void Main(string[] args)
         {
             try
@@ -25,11 +24,102 @@ namespace Twicme.Budget.Cli
         private static int Run(string[] args)
         {
             var app = new CommandLineApplication();
-            app.HelpOption(HelpFlagTemplate);
+            var budget = app.Command("budget", config =>
+            {
+                config.HelpOption(HelpFlagTemplate.Value);  
+            });
 
-            var budget = new BudgetCommandBuilder(app).Build();
+            budget.Command("create", config =>
+            {
+                config.Name = "create";
+                config.Description = "create budget providing year, month and currency";
+                config.HelpOption(HelpFlagTemplate.Value);     
+                
+                var yearOption = YearOption.Create(config);
+                var monthOption = MonthOption.Create(config);
+                var currencyOption = CurrencyOption.Create(config);
+                
+                config.OnExecute(() =>
+                {
+                 if (yearOption.NotExists || monthOption.NotExists || currencyOption.NotExists)
+                    {
+                        config.ShowHelp();
+                        return ErrorCode.Value;
+                    }
+
+                    var budgetFilesFactory = new BudgetFilesFactory();
+                    var (plannedBudgetFile, realBudgetFile) = budgetFilesFactory.Create(yearOption, monthOption, currencyOption);
             
-            new CreateBudgetCommandBuilder(budget).Build();
+                    if (plannedBudgetFile.NotExists || realBudgetFile.NotExists)
+                    {
+                        plannedBudgetFile.Save();
+                        realBudgetFile.Save();
+                
+                        Console.WriteLine($"Budget {plannedBudgetFile.Budget} has been created.");
+                        return OkCode.Value;
+                    }
+            
+                    Console.WriteLine($"Budget {plannedBudgetFile.Budget} already exists.");
+                    
+                    return OkCode.Value;
+                });
+            });
+            
+            budget.Command("add", config =>
+            {
+                config.Name = "add";
+                config.Description = "add revenue or expense to budget";
+                config.HelpOption(HelpFlagTemplate.Value);
+                
+                var yearOption = YearOption.Create(config);
+                var monthOption = MonthOption.Create(config);
+                var currencyOption = CurrencyOption.Create(config);
+                var amountOption = AmountOption.Create(config);
+                var categoryOption = CategoryOption.Create(config);
+                var descriptionOption = DescriptionOption.Create(config);
+                
+                config.OnExecute(() =>
+                {
+                    if (yearOption.NotExists ||
+                        monthOption.NotExists ||
+                        currencyOption.NotExists ||
+                        amountOption.NotExists ||
+                        categoryOption.NotExists ||
+                        descriptionOption.NotExists)
+                    {
+                        config.ShowHelp();
+                        return ErrorCode.Value;
+                    }
+                    
+                    var budgetFilesFactory = new BudgetFilesFactory();
+                    var (plannedBudgetFile, realBudgetFile) = budgetFilesFactory.Create(yearOption, monthOption, currencyOption);
+            
+                    if (realBudgetFile.NotExists)
+                    {
+                        Console.WriteLine($"Budget {realBudgetFile.Budget} does not exit. Created new budget first.");
+                        return ErrorCode.Value;
+                    }
+
+                    var budgetFile = realBudgetFile.Load();
+
+                    var category = Category.Create(categoryOption.Value);
+                    var description = new Description(descriptionOption.Value);
+                    var currency = Currency.Create(currencyOption.Value);
+                    var amount = Amount.Create(amountOption.Value, currency);
+      
+                    budgetFile.Budget.WithExpense(new Money(amount, category, DateTimeOffset.UtcNow, description));
+                    budgetFile.Save();
+                    
+                    Console.WriteLine("Expense added.");
+                    return OkCode.Value;
+                });
+                
+            });
+            
+
+//            var budget = new BudgetCommandBuilder(app).Build();
+//            
+//            new CreateBudgetCommandBuilder(budget).Build();
 //            new BudgetsListCommandBuilder(budget).Build();
 //            new AddCommandBuilder(budget).Build();
 
